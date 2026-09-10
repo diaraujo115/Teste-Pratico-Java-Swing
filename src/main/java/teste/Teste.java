aaaa9f09b643dd3bed2b7e212512d1b5bf8f9c27
Diego Araujo <araujobarbosa.15@gmail.com>
2026-09-10 18:53:04 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/teste/Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package teste;
+
+/**
+ *
+ * @author Diego
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
