8f2f413ba4aa63195316247438461513a706344a
Diego Araujo <araujobarbosa.15@gmail.com>
2026-09-10 19:26:21 -0300

    feat: adiciona models Usuario e Funcionario

--- src/main/java/teste/model/Funcionario.java
+++ src/main/java/teste/model/Funcionario.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package teste.model;
 
-/**
- *
- * @author Diego
- */
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Funcionario {
+    private int id;
+    private String nome;
+    private LocalDate dataAdmissao;
+    private BigDecimal salario;
+    private boolean status;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, LocalDate dataAdmissao, BigDecimal salario, boolean status) {
+        this.nome = nome;
+        this.dataAdmissao = dataAdmissao;
+        this.salario = salario;
+        this.status = status;
+    }
+
+    public Funcionario(int id, String nome, LocalDate dataAdmissao, BigDecimal salario, boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.dataAdmissao = dataAdmissao;
+        this.salario = salario;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
+    
     
 }
--- src/main/java/teste/model/Usuario.java
+++ src/main/java/teste/model/Usuario.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package teste.model;
 
-/**
- *
- * @author Diego
- */
 public class Usuario {
-    
+    private int id;
+    private String nome;
+    private String email;
+    private String senhaHash;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, String senhaHash) {
+        this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+    }
+
+    public Usuario(int id, String nome, String email, String senhaHash) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
 }
